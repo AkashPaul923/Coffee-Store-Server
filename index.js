5f072bbc622ae5ec8fde8bceb32ac9415e0fadc2
AkashPaul923 <akashpaul923@gmail.com>
2024-12-01 22:02:24 +0600

    data add and read

--- index.js
+++ index.js
@@ -24,12 +24,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeeCollection = client.db("coffeeDB").collection("coffees")
+
+    app.get("/coffees", async (req,res) =>{
+      const cursor = coffeeCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.post("/coffees", async (req, res)=>{
+      newCoffee= req.body
+      // console.log(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee)
+      console.log( result );
+      res.send( result )
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
